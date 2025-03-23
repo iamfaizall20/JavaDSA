1c254515af4ad58984ceb15aa8570648fe46865a
Faizal Hassan <161876313+iamfaizall20@users.noreply.github.com>
2025-03-23 07:04:03 +0000

    Add files via upload

---
+++ Is Array Sorted/Main.java
@@ -0,0 +1,19 @@
+// Problem: Is Array Sorted
+public class Main {
+
+    static boolean isArraySorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+            if (arr[i] >= arr[i+1])
+                return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 6, 5};
+
+        boolean isSorted = isArraySorted(arr);
+
+        System.out.println( (isSorted) ? "Array is Sorted" : "Array isn't Sorted");
+    }
+}
